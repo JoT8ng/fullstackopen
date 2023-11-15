e279227707b54d4d82e1a2fcb776a531185b3a72
Jocelyn Tang <jocelynt8ng@gmail.com>
2023-11-15 12:08:16 +0000

    added part4 bloglist

---
+++ part4/bloglist/controllers/blogsRouter.js
@@ -0,0 +1,64 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/models')
+
+blogsRouter.get('/', (request, response, next) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  if (!body.title || !body.author || !body.url) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  } else if (!body.likes) {
+    body.likes = 0
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
